25b16e531b7c224a4eb89d5357b0fcc441a6d2f3
GesTherapy <contact@GesTherapy.com>
2014-03-28 21:48:54 -0400

    Luis' code

--- Hack Project/src/com/example/namelater/MainActivity.java
+++ Hack Project/src/com/example/namelater/MainActivity.java
@@ -1,24 +1,45 @@
 package com.example.namelater;
 
+import java.sql.Time;
+
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.content.SharedPreferences;
+import android.widget.Toast;
+import android.view.View.OnClickListener;
+import android.util.Log;
+import android.media.MediaRecorder;
+
+// Google Drive imports
+import com.google.api.client.extensions.android.http.AndroidHttp;
+import com.google.api.client.googleapis.extensions.android.gms.auth.GoogleAccountCredential;
+import com.google.api.client.googleapis.extensions.android.gms.auth.UserRecoverableAuthIOException;
+import com.google.api.client.http.FileContent;
+import com.google.api.client.json.gson.GsonFactory;
+import com.google.api.services.drive.Drive;
+import com.google.api.services.drive.DriveScopes;
+import com.google.api.services.drive.model.File;
 
 public class MainActivity extends Activity {
 
+	private static final String LOG_TAG = "AudioRecordTest";
+    private static String mFileName = null;
+    
+    private MediaRecorder mRecorder = null;
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        /*Android.Developer.com*/
         //To create a notification
         NotificationCompat.Builder mBuilder =
                 new NotificationCompat.Builder(this)
                 .setSmallIcon(R.drawable.notification_icon)
                 .setContentTitle("My notification")
                 .setContentText("App Running");
-        // Random commit
+        // Random commit,
         
         // Creates an explicit intent for an Activity in your app
         Intent resultIntent = new Intent(this, MainActivity.class);
@@ -43,7 +64,87 @@ public class MainActivity extends Activity {
         // mId allows you to update the notification later on.
         mNotificationManager.notify(mId, mBuilder.build());
      /* */
+        
+        Time now = new Time(Time.getCurrentTimezone());
+    	now.setToNow();
+    	
+    	String stamp = now.getMonth();
+    	
+    	mFileName = Environment.getExternalStorageDirectory().getAbsolutePath();
+        mFileName += "/Audio_" + stamp ".3gp";
+        
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+        // Check for first run of the program
+        SharedPreferences sp = getPreferences(MODE_PRIVATE);
+        // If this is the first time the app is running
+        // Allow the user to register Google Drive email
+        if (sp.getFloat(lastRunVersion, 0) == 0)
+        {
+			// Store the latest run version
+			SharedPreferences.Editor edit = sp.edit();
+			edit.putFloat(lastRunVersion, (float) 1.1);
+			edit.commit();
+			// Initial instructions to user at start up
+			Toast.makeText(this,
+					"Don't feel victimized with nothing you can do!",
+					Toast.LENGTH_LONG).show();
+			Toast.makeText(this,
+					"Sign in initially and use this app to record what happens!",
+					Toast.LENGTH_LONG).show();
+		}
+        // Else, start recording audio
+        else
+        {
+        	mRecorder = new MediaRecorder();
+            mRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+            mRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+            mRecorder.setOutputFile(mFileName);
+            mRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+
+            try {
+                mRecorder.prepare();
+            } catch (IOException e) {
+                Log.e(LOG_TAG, "prepare() failed");
+            }
+
+            mRecorder.start();
+        }
      }
+    
+    /*private void saveFileToDrive()
+    {
+    	Thread t = new Thread(new Runnable() {
+    		@Override
+    		public void run() {
+    			try {
+    				// File's binary content
+    				java.io.File fileContent = new java.io.File(fileUri.getPath());
+    				FileContent mediaContent = new FileContent("image/jpeg", fileContent);
+    				
+    				// File's metadata.
+    				File body = new File();
+    				body.setTitle(fileContent.getName());
+    				body.setMimeType("vid/mp4");
+    				
+    				File file = service.files().insert(body, mediaContent).execute();
+    				if (file != null)
+    				{
+    					showToast("Photo uploaded: " + file.getTitle());
+    					startCameraIntent();
+    					}
+    				}
+    			catch (UserRecoverableAuthIOException e) {
+    				startActivityForResult(e.getIntent(), REQUEST_AUTHORIZATION);
+    				}
+    			catch (IOException e) {
+    				e.printStackTrace();
+    				}
+    			}
+    		});
+    	t.start();
+    	}*/
    
 
 
